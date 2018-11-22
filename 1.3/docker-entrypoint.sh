727f606dfd79b93e5fb04abf45b7b79e3ce39c9c
ryanmac <ryan.maccarthy@gmail.com>
2018-11-22 06:28:51 -0800

    NOMINATIM_DATA_PATH 2

--- 1.3/docker-entrypoint.sh
+++ 1.3/docker-entrypoint.sh
@@ -3,9 +3,9 @@
 # Defaults
 NOMINATIM_DATA_PATH=${NOMINATIM_DATA_PATH:="/srv/nominatim/data"}
 NOMINATIM_PBF_PATH=${NOMINATIM_PBF_PATH:="maldives-latest.osm.pbf"}
-wget -O $NOMINATIM_DATA_PATH https://www.nominatim.org/data/wikipedia_article.sql.bin
-wget -O $NOMINATIM_DATA_PATH https://www.nominatim.org/data/wikipedia_redirect.sql.bin
-wget -O $NOMINATIM_DATA_PATH https://www.nominatim.org/data/gb_postcode_data.sql.gz
+wget -O "$NOMINATIM_DATA_PATH/wikipedia_article.sql.bin" https://www.nominatim.org/data/wikipedia_article.sql.bin
+wget -O "$NOMINATIM_DATA_PATH/wikipedia_redirect.sql.bin" https://www.nominatim.org/data/wikipedia_redirect.sql.bin
+wget -O "$NOMINATIM_DATA_PATH/gb_postcode_data.sql.gz" https://www.nominatim.org/data/gb_postcode_data.sql.gz
 
 # Allow user accounts read access to the data
 chmod 755 $NOMINATIM_DATA_PATH
