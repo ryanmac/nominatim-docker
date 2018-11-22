e3088e91f82cd3d847c7e2a138891d1a896e0eea
ryanmac <ryan.maccarthy@gmail.com>
2018-11-21 20:03:59 -0800

    NOMINATIM_PBF_PATH

--- 1.3/docker-entrypoint.sh
+++ 1.3/docker-entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Defaults
-NOMINATIM_DATA_PATH=${NOMINATIM_DATA_PATH:="/srv/nominatim/data"}
-NOMINATIM_DATA_LABEL=${NOMINATIM_DATA_LABEL:="data"}
-NOMINATIM_PBF_URL=${NOMINATIM_PBF_URL:="http://download.geofabrik.de/asia/maldives-latest.osm.pbf"}
+NOMINATIM_PBF_PATH=${NOMINATIM_PBF_PATH:="maldives-latest.osm.pbf"}
+wget -O /srv/nominatim/data/wikipedia_article.sql.bin https://www.nominatim.org/data/wikipedia_article.sql.bin
+wget -O /srv/nominatim/data/wikipedia_redirect.sql.bin https://www.nominatim.org/data/wikipedia_redirect.sql.bin
+wget -O /srv/nominatim/data/gb_postcode_data.sql.gz https://www.nominatim.org/data/gb_postcode_data.sql.gz
 
-
-# Retrieve the PBF file
-curl -L $NOMINATIM_PBF_URL --create-dirs -o $NOMINATIM_DATA_PATH/$NOMINATIM_DATA_LABEL.osm.pbf
 # Allow user accounts read access to the data
-chmod 755 $NOMINATIM_DATA_PATH
+chmod 755 $NOMINATIM_PBF_PATH
+chmod 755 /srv/nominatim/data/wikipedia_article.sql.bin
+chmod 755 /srv/nominatim/data/wikipedia_redirect.sql.bin
+chmod 755 /srv/nominatim/data/gb_postcode_data.sql.gz
 
 # Start PostgreSQL
 service postgresql start
@@ -19,7 +20,8 @@ sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='nomin
 sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='www-data'" | grep -q 1 || sudo -u postgres createuser -SDR www-data
 sudo -u postgres psql postgres -c "DROP DATABASE IF EXISTS nominatim"
 useradd -m -p password1234 nominatim
-sudo -u nominatim /srv/nominatim/build/utils/setup.php --osm-file $NOMINATIM_DATA_PATH/$NOMINATIM_DATA_LABEL.osm.pbf --all --threads 2
+sudo -u nominatim /srv/nominatim/build/utils/setup.php --osm-file $NOMINATIM_PBF_PATH --all --threads 2
+sudo -u nominatim /srv/nominatim/build/utils/update.php --recompute-word-counts
 
 
 # Tail Apache logs
